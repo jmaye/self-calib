354487a4126e6e4699b02463eceaadd3bdc9b520
Jerome Maye <jerome.maye@mavt.ethz.ch>
2012-11-01 12:35:01 +0000

    update code

--- code/matlab/2d-lrf/evaluate.m
+++ code/matlab/2d-lrf/evaluate.m
@@ -17,7 +17,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % This script performs systematic evaluation on the sine wave path with
-% varying amplitudes.
+% varying amplitudes of EKF, LS, and TQR-MI
 
 clear all;
 
@@ -25,7 +25,7 @@ clear all;
 params;
 
 % number of repetitions for each amplitude
-rep = 50;
+rep = 100;
 
 % sine wave frequency
 frequency = 0.01;
@@ -33,63 +33,45 @@ frequency = 0.01;
 % landmarks parameters
 nl = 17;
 minX = 0;
-maxX = 15;
+maxX = 30;
 minY = 0;
-maxY = 15;
+maxY = 30;
 
 % amplitude parameters
 minAmplitude = 0;
-maxAmplitude = 3;
+maxAmplitude = 5;
 amplitudeStep = 0.5;
 
 % output for each repetition with a given amplitude
-Theta_tqr = zeros(rep, 3);
-Sigma_tqr = zeros(3, 3, rep);
+Theta_tqr_mi = zeros(rep, 3);
+Sigma_tqr_mi = zeros(3, 3, rep);
+batchSize_rep = zeros(rep, 1);
 Theta_ls = zeros(rep, 3);
 Sigma_ls = zeros(3, 3, rep);
 Theta_ekf = zeros(rep, 3);
 Sigma_ekf = zeros(3, 3, rep);
-R1_rep = zeros(rep, 100);
-R2_rep = zeros(rep, 100);
 
 % amplitude index
 ampIdx = 1;
 
 % optimization parameters
 optTol = 1e-6;
-maxIter = 15;
-rankTol = 0.016;
+maxIter = 20;
+rankGap = 0.02;
+batchSize = 150;
+miThreshold = 1.5;
 
 % initial guess for Theta
 Theta_hat = [0.23; 0.11; 0.8];
+
+% simulation steps
+steps = 5000;
 
 warning off all;
 
 % perform evaluation
 for amplitude = minAmplitude:amplitudeStep:maxAmplitude
   amplitude
-
-  if amplitude == 0
-    rankTol = 0.016;
-  end
-  if amplitude == 0.5
-    rankTol = 0.016;
-  end
-  if amplitude == 1.0
-    rankTol = 0.016;
-  end
-  if amplitude == 1.5
-    rankTol = 0.015;
-  end
-  if amplitude == 2.0
-    rankTol = 0.015;
-  end
-  if amplitude == 2.5
-    rankTol = 0.013;
-  end
-  if amplitude == 3.0
-    rankTol = 0.011;
-  end
 
   for i = 1:rep
     i
@@ -103,24 +85,24 @@ for amplitude = minAmplitude:amplitudeStep:maxAmplitude
     x_odom = odomInt([x_true(1), y_true(1), th_true(1)], [v, om], t);
     l_hat = initLandmarks(x_odom, Theta_hat, r, b);
 
-    % TQR optimization
-    disp('TQR');
-    [x_est l_est Theta_est Sigma] = ls_slam_calib(x_odom, l_hat, ...
-       Theta_hat, [v, om], r, b, t, Q, R, maxIter, optTol, rankTol);
-    Theta_tqr(i, :) = Theta_est';
-    Sigma_tqr(:, :, i) = Sigma;
+    % TQR-MI optimization
+    disp('TQR-MI');
+    [x_est l_est Theta_est Sigma batchIdx] = ls_slam_calib_it_auto(x_odom, ...
+      l_hat, Theta_hat, [v, om], r, b, t, Q, R, maxIter, optTol, batchSize, ...
+      miThreshold, rankGap);
+    Theta_tqr_mi(i, :) = Theta_est';
+    Sigma_tqr_mi(:, :, i) = Sigma;
+    batchSize_rep(i) = length(batchIdx);
     Theta_est
     Sigma
 
     % Non-linear least squares without regularization
     disp('LS');
-    [x_est l_est Theta_est Sigma R1 R2] = ls_slam_calib_prior(x_odom, l_hat, ...
+    [x_est l_est Theta_est Sigma] = ls_slam_calib_prior(x_odom, l_hat, ...
       Theta_hat, [v, om], r, b, t, Q, R, maxIter, optTol, x0, ...
       diag([1e-6; 1e-6; 1e-6]));
     Theta_ls(i, :) = Theta_est';
     Sigma_ls(:, :, i) = Sigma;
-    R1_rep(i, :) = R1(end - 99:end);
-    R2_rep(i, :) = R2(end - 99:end);
     Theta_est
     Sigma
 
@@ -135,22 +117,23 @@ for amplitude = minAmplitude:amplitudeStep:maxAmplitude
   end
 
   % save results for this amplitude
-  ampRes(ampIdx).amplitude = amplitude;
-  ampRes(ampIdx).rep = rep;
-  ampRes(ampIdx).R1_rep = R1_rep;
-  ampRes(ampIdx).R2_rep = R2_rep;
-  ampRes(ampIdx).Theta_tqr = Theta_tqr;
-  ampRes(ampIdx).Sigma_tqr = Sigma_tqr;
-  ampRes(ampIdx).Theta_ls = Theta_ls;
-  ampRes(ampIdx).Sigma_ls = Sigma_ls;
-  ampRes(ampIdx).Theta_ekf = Theta_ekf;
-  ampRes(ampIdx).Sigma_ekf = Sigma_ekf;
+  ampResTQRMI(ampIdx).amplitude = amplitude;
+  ampResTQRMI(ampIdx).rep = rep;
+  ampResTQRMI(ampIdx).Theta_tqr_mi = Theta_tqr_mi;
+  ampResTQRMI(ampIdx).Sigma_tqr_mi = Sigma_tqr_mi;
+  ampResTQRMI(ampIdx).batchSize_rep = batchSize_rep;
+  ampResTQRMI(ampIdx).Theta_ls = Theta_ls;
+  ampResTQRMI(ampIdx).Sigma_ls = Sigma_ls;
+  ampResTQRMI(ampIdx).Theta_ekf = Theta_ekf;
+  ampResTQRMI(ampIdx).Sigma_ekf = Sigma_ekf;
 
   % save file to disk
-  save('ampRes.mat', 'ampRes');
+  save('ampResTQRMI.mat', 'ampResTQRMI');
 
   % increment amplitude index
   ampIdx = ampIdx + 1;
 end
+
+
 
 warning on all;
